fd326b90f6bbac24d36360d074cd5b89499a5e20
Bourama coulibaly <bouramadevweb@gmail.com>
2024-01-23 15:15:23 +0100

    fonction run_calculation_app

--- calculatrice.py
+++ calculatrice.py
@@ -18,5 +18,25 @@ def on_button_click(result_var, operator):
     else:
         # Ajouter l'opérateur au résultat actuel
         result_var.set(current_text + operator)
+def run_calculator_app():
+    root = tk.Tk()
+    root.title("Calculatrice")
 
+    result_var = tk.StringVar()
+    result_entry = tk.Entry(root, textvariable=result_var, justify="right", font=("Arial", 14))
+    result_entry.grid(row=0, column=0, columnspan=3, sticky="nsew")
+
+    # Boutons pour les opérations de base
+    create_button(root, '+', 1, 0, lambda: on_button_click(result_var, '+'))
+    create_button(root, '-', 1, 1, lambda: on_button_click(result_var, '-'))
+    create_button(root, '=', 1, 2, lambda: on_button_click(result_var, '='))
+
+    for i in range(1, 3):
+        root.grid_rowconfigure(i, weight=1)
+        root.grid_columnconfigure(i, weight=1)
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    run_calculator_app()
 
